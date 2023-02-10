3b2f7d79b400e9123cabee7bae8c6ddb6a692ddb
RyoDeveloper <121738322+RyoDeveloper@users.noreply.github.com>
2023-02-10 23:54:39 +0900

    Add: WebView

---
+++ WebViewTemplate/WebView.swift
@@ -0,0 +1,21 @@
+//
+//  WebView.swift
+//  WebViewTemplate
+//
+//  https://github.com/RyoDeveloper/WebViewTemplate
+//  Copyright © 2023 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView()
+    }
+}
