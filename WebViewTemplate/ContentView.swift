ea8d3003e4e4631342a928a582af331bc6405389
RyoDeveloper <121738322+RyoDeveloper@users.noreply.github.com>
2023-02-10 23:53:04 +0900

    Initial Commit

---
+++ WebViewTemplate/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WebViewTemplate
+//
+//  https://github.com/RyoDeveloper/WebViewTemplate
+//  Copyright © 2023 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebViewTemplate/WebViewTemplateApp.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewTemplateApp.swift
+//  WebViewTemplate
+//
+//  https://github.com/RyoDeveloper/WebViewTemplate
+//  Copyright © 2023 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WebViewTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
